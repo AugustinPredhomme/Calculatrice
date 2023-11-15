2f14e52f9662205eb39f2a0051bc8eea91521ec1
Augustin <raygister@gmail.com>
2023-11-15 10:04:31 +0100

    Etape 2 : Méthodes pour les opérations de base

--- script.js
+++ script.js
@@ -8,4 +8,28 @@ class Calculator {
     reset() {
         this.result = 0;
     }
+
+    add() {
+        this.result = this.num1 + this.num2;
+    }
+
+    subtract() {
+        this.result = this.num1 - this.num2;
+    }
+
+    multiply() {
+        this.result = this.num1 * this.num2;
+    }
+
+    divide() {
+    if (this.num2 !== 0) {
+        this.result = this.num1 / this.num2;
+    } else {
+        console.error("Division by zero is not allowed.");
+    }
+    }
+
+    getResult() {
+        return this.result;
+    }
 }
