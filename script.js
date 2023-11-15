400196e2e32cb97c3c9bf2dcd1295a49e6893d2d
Augustin <raygister@gmail.com>
2023-11-15 10:01:16 +0100

    Etape 1:Création classe calculator & méthode reset

---
+++ script.js
@@ -0,0 +1,11 @@
+class Calculator {
+    constructor(num1, num2) {
+      this.num1 = num1;
+      this.num2 = num2;
+      this.result = 0;
+    }
+  
+    reset() {
+        this.result = 0;
+    }
+}
