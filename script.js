85ff25d1ccbc918f29273bab4a0ef5837ad7444e
Augustin <raygister@gmail.com>
2023-11-15 10:07:00 +0100

    Etape 3 : Tests de la calculatrice

--- script.js
+++ script.js
@@ -33,3 +33,20 @@ class Calculator {
         return this.result;
     }
 }
+
+const myCalculator = new Calculator(5, 3);
+
+myCalculator.add();
+console.log("Addition : " + myCalculator.getResult());
+
+myCalculator.subtract();
+console.log("Subtraction : " + myCalculator.getResult());
+
+myCalculator.multiply();
+console.log("Multiplication : " + myCalculator.getResult());
+
+myCalculator.divide();
+console.log("Division : " + myCalculator.getResult());
+
+myCalculator.reset();
+console.log("Result after reset : " + myCalculator.getResult());
