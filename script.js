4e3b08cb64f0586825734785684edcbdeba3c584
Augustin <raygister@gmail.com>
2023-11-15 10:48:47 +0100

    Affichage à l'écran du résultat

--- script.js
+++ script.js
@@ -3,50 +3,58 @@ class Calculator {
       this.num1 = num1;
       this.num2 = num2;
       this.result = 0;
+      this.resultDisplay = document.getElementById('result-display');
     }
   
     reset() {
         this.result = 0;
+        this.updateDisplay()
     }
 
     add() {
         this.result = this.num1 + this.num2;
+        this.updateDisplay();
     }
 
     subtract() {
         this.result = this.num1 - this.num2;
+        this.updateDisplay();
     }
 
     multiply() {
         this.result = this.num1 * this.num2;
+        this.updateDisplay();
     }
 
     divide() {
     if (this.num2 !== 0) {
         this.result = this.num1 / this.num2;
+        this.updateDisplay();
     } else {
         console.error("Division by zero is not allowed.");
     }
     }
 
-    getResult() {
-        return this.result;
+    updateDisplay(){
+        this.resultDisplay.textContent = "Résultat : " + this.result;
     }
 }
 
 const myCalculator = new Calculator(5, 3);
 
 myCalculator.add();
-console.log("Addition : " + myCalculator.getResult());
+console.log("Addition: " + myCalculator.result);
 
 myCalculator.subtract();
-console.log("Subtraction : " + myCalculator.getResult());
+console.log("Substraction: " + myCalculator.result);
 
 myCalculator.multiply();
-console.log("Multiplication : " + myCalculator.getResult());
+console.log("Multiplication: " + myCalculator.result);
 
 myCalculator.divide();
-console.log("Division : " + myCalculator.getResult());
+console.log("Division: " + myCalculator.result);
 
 myCalculator.reset();
-console.log("Result after reset : " + myCalculator.getResult());
+console.log("Reset: " + myCalculator.result);
+
+myCalculator.updateDisplay()
